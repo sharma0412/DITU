4eb114377ae50155d3a461bc16f58e99b060508b
Pankaj Sharma <sharma0412pankaj@gmail.com>
2023-06-26 06:04:44 -0400

    UI issue fixed

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/ProductList.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/ProductList.java
@@ -4,33 +4,31 @@ public class ProductList {
     String productList[] = {"Zara", "parda"};
 
     public static void main(String args[]) {
-        int arr[] = {50, 60, 80, 90};
+            try{
+                for (int i = 1; i <= 10; i++) {
+                    String numberString = String.valueOf(i);
+                    System.out.println(numberString);
+                }
+                int[] age = {12, 4, 5};
+                System.out.println("using for loop:");
+                for (int i = 0; i < age.length; i++) {
+                    System.out.println(age[i]);
+                }
 
-        for (int i = 0; i <= arr.length; i++) {
-            System.out.println(arr[i]);
+                age = new int[5];
+                age[0] = 12;
+                age[1] = 4;
+                age[2] = 5;
+                System.out.println("numberString");
+            }catch (ArrayIndexOutOfBoundsException e) {
+
+                System.out.println(e);
+            }
 
         }
 
-            for (int i = 1; i <= 10; i++) {
-                String numberString = String.valueOf(i);
-                System.out.println(numberString);
-            }
-            int[] age = {12, 4, 5};
-            System.out.println("using for loop:");
-            for (int i = 0; i < age.length; i++) {
-                System.out.println(age[i]);
-            }
-
-            age = new int[5];
-            age[0] = 12;
-            age[1] = 4;
-            age[2] = 5;
-            System.out.println("numberString");
-
-
-
     }
-}
+
 
 
 
