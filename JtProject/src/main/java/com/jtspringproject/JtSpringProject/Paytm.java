378b011e24833fa3ec0e80ff91a854278f655eea
sharma0412 <sharma0412pankaj@gmail.com>
2023-07-19 19:06:37 +0530

    service change

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/Paytm.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/Paytm.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class Paytm {
 
-    public static void toBinary(int decimal){
+    public static void toBinary(int decimal) {
         int binary[] = new int[40];
         int index = 0;
-        while(decimal > 0){
-            binary[index++] = decimal%2;
-            decimal = decimal/2;
+        while (decimal > 0) {
+            binary[index++] = decimal % 2;
+            decimal = decimal / 2;
         }
-        for(int i = index-1;i >= 0;i--){
+        for (int i = index - 1; i >= 0; i--) {
             System.out.print(binary[i]);
         }
         System.out.println();//new line
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Decimal of 10 is: ");
         toBinary(10);
         System.out.println("Decimal of 21 is: ");
@@ -25,35 +25,31 @@ public class Paytm {
         System.out.println("Decimal of 31 is: ");
         toBinary(31);
 
-        System.out.println(Integer.parseInt("a",16));
-        System.out.println(Integer.parseInt("f",16));
-        System.out.println(Integer.parseInt("121",16));
+        System.out.println(Integer.parseInt("a", 16));
+        System.out.println(Integer.parseInt("f", 16));
+        System.out.println(Integer.parseInt("121", 16));
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n to compute the nth prime number: ");
 //reading an integer from the user
         int n = sc.nextInt();
-        int num=1, count=0, i;
-        while (count < n)
-        {
-            num=num+1;
-            for (i = 2; i <= num; i++)
-            {
+        int num = 1, count = 0, i;
+        while (count < n) {
+            num = num + 1;
+            for (i = 2; i <= num; i++) {
 //determines the modulo and compare it with 0
-                if (num % i == 0)
-                {
+                if (num % i == 0) {
 //breaks the loop if the above condition returns true
                     break;
                 }
             }
-            if (i == num)
-            {
+            if (i == num) {
 //increments the count variable by 1 if the number is prime
-                count = count+1;
+                count = count + 1;
             }
         }
 //prints the nth prime number
-        System.out.println("The " +n +"th prime number is: " + num);
+        System.out.println("The " + n + "th prime number is: " + num);
     }
 
 };
